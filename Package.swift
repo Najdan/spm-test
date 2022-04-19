6428d6efb3ee488799a62c4a38526afdd8ab9024
Najdan Tomić <najdan.tomic@gmail.com>
2022-04-19 17:08:11 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(name: "LRAtsSDK",
+                      products: [
+                          // Products define the executables and libraries a package produces, and make them visible to other packages.
+                          .library(
+                                  name: "LRAtsSDK",
+                                  targets: ["LRAtsSDK"]),
+                      ],
+                      dependencies: [],
+                      targets: [
+                          .binaryTarget(name: "LRAtsSDK", url: "https://ats-sdk-ios-prod.launch.liveramp.com/1.0.0-beta5/LRAtsSDK.zip", checksum: "3ac594581b9ba5ba9897ff42562b6c9e310ac164580b93b4035954b7f5fd637a")
+                      ]
+)
